4f8c45df10b23cb2e55a8397618f9669afde4278
aym003 <aym003.hit@gmail.com>
2020-03-24 15:56:41 +0100

    aman comm

---
+++ Assets/PlayerMoter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerMoter : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private float jumpForce = 4.0f;
+    private float gravity = 12.0f;
+    private float verticalVelocity;
+    private float speed = 7.0f;
+    private float LANE_DISTANCE = 3.0f;
+    private int desiredLane = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            MoveLane(false);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            MoveLane(true);
+
+        Vector3 targetPosition = transform.position.z * Vector3.forward;
+        if (desiredLane == 0)
+            targetPosition += Vector3.left * LANE_DISTANCE;
+        else if(desiredLane==2)
+            targetPosition += Vector3.right * LANE_DISTANCE;
+
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = (targetPosition - transform.position).normalized.x * speed;
+        moveVector.y = -0.1f;
+        moveVector.z = speed;
+
+
+        controller.Move(moveVector * Time.deltaTime);
+
+
+    }
+    void MoveLane(bool goingRight)
+    {
+        desiredLane += (goingRight) ? 1 : -1;
+        desiredLane = Mathf.Clamp(desiredLane, 0, 2);
+    }
+}
--- Assets/TruckController.cs
+++ Assets/TruckController.cs
@@ -19,34 +19,39 @@ private int desiredLane = 1;
     // Update is called once per frame
     void Update()
     {
-     var rnd = Random.Range(0,2);
-	 int direction  = rnd;   
-	 if (direction == 0)
-		 MoveLane(false);
-	 if (direction ==1 )
-		 MoveLane(true);
+        var rnd = Random.Range(0, 2);
+        int direction = rnd;
+        if (direction == 0)
+            MoveLane(false);
+        if (direction == 1)
+            MoveLane(true);
 
- 
-	 Vector3 targetPosition = transform.position.z * Vector3.forward;
-	targetPosition.y = 0.0f;
-	 if (desiredLane == 0){
-		 targetPosition += Vector3.left * LANE_DISTANCE;
-}
-	 else if (desiredLane ==2)
-		 targetPosition += Vector3.right * LANE_DISTANCE;
-    Vector3 moveVector = Vector3.zero;
-	moveVector.x = (targetPosition - transform.position).normalized.x * speed;
-	moveVector.y = 0.0f;
-	moveVector.z = speed;
-	controller.Move(moveVector * Time.deltaTime);
-	var boolSmeed = Random.Range(0,10001);
-	int boolSm = boolSmeed;
-Vector3 test = targetPosition;
-	if (boolSm == 2)
-		
-		test.y = 0.1f;
-		SpawnSmeedAtPosition(test);
 
+        Vector3 targetPosition = transform.position.z * Vector3.forward;
+        targetPosition.y = 0.0f;
+        if (desiredLane == 0)
+        {
+            targetPosition += Vector3.left * LANE_DISTANCE;
+        }
+        else if (desiredLane == 2)
+            targetPosition += Vector3.right * LANE_DISTANCE;
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = (targetPosition - transform.position).normalized.x * speed;
+        moveVector.y = 0.0f;
+        moveVector.z = speed;
+        controller.Move(moveVector * Time.deltaTime);
+        var boolSmeed = Random.Range(0, 1);
+        int boolSm = boolSmeed;
+        double result = new System.Random().Next(0,100);
+        System.Console.Write(result);
+        Vector3 test = targetPosition;
+        if (result == 1) {
+            
+        if (boolSm == 2)
+
+            test.y = 0.1f;
+        SpawnSmeedAtPosition(test);
+    }
 	}
 	private void MoveLane(bool goingRight){
 		desiredLane += (goingRight) ? 1 : -1;
@@ -54,5 +59,7 @@ Vector3 test = targetPosition;
 	}
 	private void SpawnSmeedAtPosition(Vector3 spawnPosition){
 	GameObject smeed = Instantiate(_smeed, spawnPosition, Quaternion.identity);
-	}
+        smeed.transform.localScale= Vector3.Scale(transform.localScale, new Vector3(10, 10, 10));
+
+    }
 }
